0fb410068f2dfba7a3d2f2b09fcc00185ff98809
MWPX777 <mwpx777@gmail.com>
2021-01-18 17:29:02 -0600

    added colums for 5 day forcast

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ let seachButton = document.querySelector("#searchButton")
 let searchForm = document.querySelector("#searchForm")
 let cardText = document.querySelector("#cardText")
 let citiesContainer = document.querySelector("#citiesContainer")
+let currentCityName = document.querySelector("#currentCityName")
 
 function formSubmitHandler(event){
     event.preventDefault();
     // get value from input element
     var searchFormText = searchForm.value.trim();
+    
     if (searchFormText){
-        console.log(searchFormText)
+        // console.log(searchFormText)
+        currentCityName.textContent = searchFormText
         //this will pass searchForm.value into getCurrentWeather function as argument
         //  getCurrentWeather(searchFormText);
         //  add div tag to search for city column
         // create new <h5> tag
         var cityNameText = document.createElement("h5");
         // give <div> a class
-        cityNameText.classList="form-control btn btn-outline-secondary align-center"
+        cityNameText.classList="form-control btn btn-outline-dark align-center"
         // add text to the element
         cityNameText.textContent=searchFormText;
         // append to list
